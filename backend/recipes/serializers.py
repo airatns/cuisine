0e92fd7f016e94ddcd261c04f89f500f85cd4eab
Airat Sakhibgareev <mr-airat@yandex.ru>
2022-08-28 01:13:14 +0300

    added recipe block

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -5,22 +5,32 @@ from users.serializers import UserListSerializer
 from users.models import User
 from users.serializers import UserListSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 from drf_extra_fields.fields import Base64ImageField
-
+from rest_framework.response import Response
 
 class TagSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, в каком формате ожидаем увидеть
+    данные по Тегам, извлеченные из БД.
+    """
     class Meta:
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, в каком формате ожидаем увидеть
+    данные по Ингредиентам, извлеченные из БД.
+    """
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
 
 
 class TagID2ObjectSerializer(serializers.RelatedField):
+    """Сериализатор переопределяет, в каком формате ожидаем увидеть
+    данные по Тегам, извлеченные из БД.
+    """
     def to_internal_value(self, data):
         return data
 
@@ -34,6 +44,9 @@ class TagID2ObjectSerializer(serializers.RelatedField):
 
 
 class IngredientForRecipeListSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, в каком формате ожидаем увидеть
+    данные по Ингредиентам для Рецепта, извлеченные из БД.
+    """
     id = serializers.IntegerField(source='ingredient.id', read_only=True)
     name = serializers.CharField(source='ingredient.name', read_only=True)
     measurement_unit = serializers.CharField(
@@ -48,6 +61,9 @@ class IngredientForRecipeListSerializer(serializers.ModelSerializer):
 
 
 class IngredientForRecipeCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, какие поля будут заполнены
+    по Ингредиентам для Рецепта при добавлении нового объекта в БД и их типы.
+    """
     id = serializers.IntegerField(write_only=True)
     quantity = serializers.IntegerField(write_only=True)
 
@@ -57,6 +73,9 @@ class IngredientForRecipeCreateSerializer(serializers.ModelSerializer):
 
 
 class RecipeListSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, в каком формате ожидаем увидеть
+    данные по Рецептам, извлеченные из БД.
+    """
     tags = TagSerializer(many=True, read_only=True)
     author = UserListSerializer(read_only=True)
     ingredients = IngredientForRecipeListSerializer(
@@ -79,15 +98,16 @@ class RecipeListSerializer(serializers.ModelSerializer):
                   'text',
                   'cooking_time',
         )
-        read_only_fields = ('author',)
 
 
 class RecipeCreateSerializer(serializers.ModelSerializer):
+    """Сериализатор определяет, какие поля будут заполнены
+    по Рецептам при добавлении нового объекта в БД и их типы.
+    """
     tags = TagID2ObjectSerializer(many=True, queryset=Tag.objects.all())
     author = UserListSerializer(read_only=True)
     ingredients = IngredientForRecipeCreateSerializer(many=True)
     # image = Base64ImageField()
-
 
     class Meta:
         model = Recipe
@@ -102,10 +122,10 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
                   'text',
                   'cooking_time',
         )
-        read_only_fields = ('author',)
-    
 
     def create(self, validated_data):
+        """Метод по созданию нового объекта Рецептов (POST-запрос).
+        """
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
         recipe = Recipe.objects.create(**validated_data)
@@ -120,13 +140,55 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
                 ingredient=current_ingredient,
                 quantity=quantity,
             )
+        recipe.save()
         return recipe
 
+    def update(self, instance, validated_data):
+        """Метод по изменению объекта Рецептов (PATCH-запрос).
+        """
+        tags = validated_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        for item in validated_data:
+            if Recipe._meta.get_field(item):
+                setattr(instance, item, validated_data[item])
+        instance.tags.set(tags)
+
+        IngredientForRecipe.objects.filter(recipe=instance).delete()
+        for ingredient in ingredients:
+            id = ingredient.get('id')
+            quantity = ingredient.get('quantity')
+            current_ingredient = get_object_or_404(Ingredient, pk=id)
+            IngredientForRecipe.objects.create(
+                recipe=instance,
+                ingredient=current_ingredient,
+                quantity=quantity,
+            )
+        instance.save()
+        return instance
 
     def to_representation(self, instance):
+        """Метод переопределяет, в каком формате ожидаем увидеть
+        данные по Рецептам, извлеченные из БД.
+        """
         serializer = RecipeListSerializer(
             instance,
             context=self.context
         )
         return serializer.data
+
+
+    def validate_name(self, value):
+        """Валидация на название Рецепта.
+        Если у данного автора уже есть Рецепт с таким названием ,
+        создание нового Рецепта невозможно.
+        """
+        if self.context['request'].method == 'POST':
+            user = self.context['request'].user
+            recipes_by_user = Recipe.objects.filter(author=user)
+            for recipe in recipes_by_user:
+                if recipe.name.lower() == value.lower():
+                    raise serializers.ValidationError(
+                        'Рецепт с таким названием уже был опубликован Вами!'
+                    )
+        return value
 
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -8,24 +8,40 @@ from .serializers import IngredientSerializer, RecipeListSerializer, RecipeCreat
 
 
 class TagViewSet(ReadOnlyModelViewSet):
+    """Вьюсет по работе с Тегами.
+    """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
+    """Вьюсет по работе с Ингредиентами.
+    """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
 
 
 class RecipeViewSet(ModelViewSet):
+    """Вьюсет по работе с Рецептами.
+    """
     queryset = Recipe.objects.all()
     serializer_class = RecipeListSerializer
 
     def get_serializer_class(self):
+        """Используем разные сериализаторы для GET и POST-запросов.
+        """
         if self.request.method == 'GET':
             return RecipeListSerializer
         return RecipeCreateSerializer
         
     def perform_create(self, serializer):
+        """В поле Author передадим объект пользователя, отправшего запрос,
+        при создании объекта Рецепта.
+        """
         serializer.save(author=self.request.user)
 
+    def perform_update(self, serializer):
+        """В поле Author передадим объект пользователя, отправшего запрос,
+        при изменении объекта Рецепта.
+        """
+        serializer.save(author=self.request.user)
