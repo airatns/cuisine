f095cfa8f1b176cf1801ca706c90393ffc4ab6c5
Airat Sakhibgareev <mr-airat@yandex.ru>
2022-10-03 13:59:11 +0300

    subscription limit

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -78,9 +78,6 @@ class SubscribeSerializer(serializers.ModelSerializer):
         ).exists()
 
     def get_recipes(self, obj):
-        # request = self.context.get('request')
-        # recipes_limit = request.GET.get('recipes_limit')
-
         author = User.objects.get(id=obj.author.id)
         recipes = Recipe.objects.filter(author=author)
         serializer = RecipeInCartSerializer(
@@ -88,8 +85,6 @@ class SubscribeSerializer(serializers.ModelSerializer):
             read_only=True,
             many=True,
         )
-        # if recipes_limit:
-        #     recipes = recipes[:int(recipes_limit)]
         return serializer.data
 
     def get_recipes_count(self, obj):
