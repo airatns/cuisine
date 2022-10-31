d970a5ea8b962a5c625e9e8166cfdc8f1fab0106
Airat Sakhibgareev <96816183+airatns@users.noreply.github.com>
2022-10-31 14:32:55 +0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -76,7 +76,7 @@ def subscribe(request, author_id):
                 'message': 'Вы уже подписаны на этого автора'
             }, status=status.HTTP_400_BAD_REQUEST)
         subscription = Subscription.objects.create(user=user, author=author)
-        serializer = SubscribeSerializer()
+        serializer = SubscribeSerializer(author)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     elif request.method == 'DELETE':
